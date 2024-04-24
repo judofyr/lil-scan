4909390d63f2a0539e7d436b1569a9d363798f9a
Magnus Holm <judofyr@gmail.com>
2024-04-24 09:16:37 +0200

    Add `zig build docs`

--- build.zig
+++ build.zig
@@ -33,6 +33,14 @@ pub fn build(b: *std.Build) !void {
     // running `zig build`).
     b.installArtifact(lib);
 
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = lib.getEmittedDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
+    const docs_step = b.step("docs", "Install documentation");
+    docs_step.dependOn(&install_docs.step);
+
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
